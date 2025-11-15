c2486de08e178e8586b769b6ce162d92bd164b38
NinerMG <maciej.gala@hotmail.com>
2025-11-16 00:13:50 +0100

    removing comment

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
 from app import app
 
-# Uruchomienie aplikacji
 if __name__ == "__main__":
-    # Debug=True tylko do developmentu, w produkcji ustaw False
     app.run(host="127.0.0.1", port=8080, debug=True)
